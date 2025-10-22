2cbff33ee1ebd8c0b5c65f4b1da3efbdf3cc6583
Utkarsh Verma <108287933+Utkarshvr@users.noreply.github.com>
2025-10-22 22:52:43 +0530

    feat: integrate Montserrat font and update layout styling

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,6 +1,17 @@
-import { Stack } from "expo-router";
+import { useFonts } from "@expo-google-fonts/montserrat";
+import { SplashScreen, Stack } from "expo-router";
+import { useEffect } from "react";
+import fonts from "../config/theme/fonts";
 import "../styles/global.css";
 
 export default function RootLayout() {
+  const [fontsLoaded] = useFonts(fonts);
+
+  useEffect(() => {
+    if (fontsLoaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
   return <Stack />;
 }
--- src/screens/FoldersScreen.tsx
+++ src/screens/FoldersScreen.tsx
@@ -3,7 +3,7 @@ import { ScrollView, Text } from "react-native";
 export default function FoldersScreen() {
   return (
     <ScrollView contentContainerClassName="flex-1 bg-white dark:bg-black p-4">
-      <Text className="text-black dark:text-white font-bold text-2xl">
+      <Text className="text-black dark:text-white text-3xl font-montSemiBold">
         Folders
       </Text>
     </ScrollView>
